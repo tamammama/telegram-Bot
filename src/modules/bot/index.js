a6c05a3d5bb75d989227e2f9d0fc4b12b912e9ee
tamammama <72872783+tamammama@users.noreply.github.com>
2022-05-05 13:45:43 +0200

    Update index.js

--- src/modules/bot/index.js
+++ src/modules/bot/index.js
@@ -5,10 +5,10 @@ module.exports = function bot(app) {
     
     app.retriver.listener.on( "new_item", ( item ) => {
         app.chats.forEach( chat => {
-            var message = `<b>${item.name}</b> is available for purchase!\nBuy now price: <b>${item.current_price}</b>\nPrice in USD: <b>$${item.dollar_price}</b>\nMarket price: <b>${item.market_price}</b>\n<b>Pick up now: </b>${app.config.website}`;
-            bot.telegram.sendMessage( chat, message, {parse_mode:"HTML"} , { 
+            var message = `<b>${item.name}</b> is available for purchase!\nBuy now price: <b>${item.current_price}</b>\nPrice in USD: <b>$${item.dollar_price}</b>\nMarket price: <b>${item.market_price}</b>\n<b>Pick up now: </b>${app.config.website} , { 
     disable_web_page_preview: true 
-} );
+}`;
+            bot.telegram.sendMessage( chat, message, {parse_mode:"HTML"}  );
         });
     });
 
