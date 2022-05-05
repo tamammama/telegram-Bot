45bfae6c91d536fbafef4815dbf81d9a5358a361
tamammama <72872783+tamammama@users.noreply.github.com>
2022-05-05 13:37:22 +0200

    Update index.js

--- src/modules/bot/index.js
+++ src/modules/bot/index.js
@@ -5,14 +5,18 @@ module.exports = function bot(app) {
     
     app.retriver.listener.on( "new_item", ( item ) => {
         app.chats.forEach( chat => {
-            var message = `<b>${item.name}</b> is available for purchase!\nBuy now price: <b>${item.current_price}</b>\nPrice in USD: <b>$${item.dollar_price}</b>\nMarket price: <b>${item.market_price}</b>\n<b>Pick up now: </b>${app.config.website}&disable_web_page_preview=True`;
+            var message = `<b>${item.name}</b> is available for purchase!\nBuy now price: <b>${item.current_price}</b>\nPrice in USD: <b>$${item.dollar_price}</b>\nMarket price: <b>${item.market_price}</b>\n<b>Pick up now: </b>${app.config.website , { 
+    disable_web_page_preview: true 
+}}`;
             bot.telegram.sendMessage( chat, message, {parse_mode:"HTML"}  );
         });
     });
 
     app.retriver.listener.on( "available_again_item", ( item ) => {
         app.chats.forEach( chat => {
-            var message = `<b>${item.name}</b> for <b>$${item.dollar_price}</b> is available again for purchase!\n<b>Pick up now: </b>${app.config.website}`;
+            var message = `<b>${item.name}</b> for <b>$${item.dollar_price}</b> is available again for purchase!\n<b>Pick up now: </b>${app.config.website , { 
+    disable_web_page_preview: true 
+}}`;
             bot.telegram.sendMessage( chat, message, {parse_mode:"HTML"}  );
         })
     });
