8165951d5291c45a17d487371747086dd2331b5f
tamammama <72872783+tamammama@users.noreply.github.com>
2022-05-06 07:15:12 +0200

    Update index.js

--- src/modules/bot/index.js
+++ src/modules/bot/index.js
@@ -6,14 +6,14 @@ module.exports = function bot(app) {
     app.retriver.listener.on( "new_item", ( item ) => {
         app.chats.forEach( chat => {
             var message = `<b>${item.name}</b> is available for purchase!\nBuy now price: <b>${item.current_price}</b>\nPrice in USD: <b>$${item.dollar_price}</b>\nMarket price: <b>${item.market_price}</b>\n<b>Pick up now: </b>${app.config.website}`;
-            bot.telegram.sendMessage( chat, message, {parse_mode:"HTML"}  );
+            bot.telegram.sendMessage( chat, message, {parse_mode:"HTML",disable_web_page_preview: true}  );
         });
     });
 
     app.retriver.listener.on( "available_again_item", ( item ) => {
         app.chats.forEach( chat => {
             var message = `<b>${item.name}</b> for <b>$${item.dollar_price}</b> is available again for purchase!\n<b>Pick up now: </b>${app.config.website}`;
-            bot.telegram.sendMessage( chat, message, {parse_mode:"HTML"}  );
+            bot.telegram.sendMessage( chat, message, {parse_mode:"HTML",disable_web_page_preview: true}  );
         })
     });
 
